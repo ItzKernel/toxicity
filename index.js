ed469bb0f011f04e75e869f90574b1324689c240
ItzKernel <78853728+ItzKernel@users.noreply.github.com>
2021-08-27 20:16:15 +0200

    Made woke command

--- index.js
+++ index.js
@@ -1,78 +1,40 @@
-/* Essential Packages */
 const { Plugin } = require('powercord/entities');
-const { inject, uninject } = require('powercord/injector');
-const { getModule } = require('powercord/webpack');
 
-/* Plugin Specific Packages */
-const { ContextMenu } = require('powercord/components');
-
-/* Settings */
-const Settings = require('./Components/Settings.jsx');
-
-module.exports = class MyPlugin extends Plugin {
+module.exports = class Toxicity extends Plugin {
     /* Entry Point */
     async startPlugin() {
-        /* CSS/SCSS - Used for styling */
-        this.loadStylesheet('style.scss');
+        powercord.api.commands.registerCommand(
+            {
+                command: "woke",
+                description: "mAkEs tExT lOoK lIkE tHiS",
+                usage: "{c} [text]",
+                executor: args => {
+                    args.pop(); args.pop();
+                    console.log(args);
 
-        /* Register Settings */
-        powercord.api.settings.registerSettings(this.entityID, {
-            category: this.entityID,
-            label: this.manifest.name, // Label that appears in the settings menu
-            render: Settings // The React component to render. In this case, the imported Settings file
-        });
+                    let inText = args.join(" ");
+                    let resultText = "";
 
-        /**
-         * The following is an example of adding an option to an image's context menu,
-         * which changes the image to a picture of a dog/cat based on the setting.
-         */
-
-        // Discord is made up of thousands of modules, the following lines look for the modules which meet the specified filters.
-        this.imageWrapper = await getModule(['imageWrapper']);
-        const injectInto = await getModule(
-            m => m.default && m.default.displayName === 'MessageContextMenu'
-        );
-
-        /**
-         * The following injects a function into the specified module.
-         * Parameter 1: The InjectionID, used to uninject (should be unique).
-         * 2: The module you want to inject into.
-         * 3: The function name you want to target.
-         * 4: The function you want to inject.
-         */
-        inject('my-plugin-image-menu', injectInto, 'default', (event, res) => {
-            const target = event[0].target;
-
-            /** Only add to the Menu when the target is an image
-             * and the parent element(module) contains an imageWrapper */
-            if (target.tagName.toLowerCase() === 'img') {
-                let displayCat = this.settings.get('displayCat');
-
-                // Push new ContextMenu item to res.props.children
-                res.props.children.push(
-                    ...ContextMenu.renderRawItems([
-                        {
-                            type: 'button',
-                            name: `Display ${displayCat ? 'Cat' : 'Dog'}`,
-                            id: `toPetButton` /* When adding items, make sure all of the id's are different */,
-                            onClick: () => {
-                                /* This is the function that is executed when the button is clicked */
-                                target.src = displayCat
-                                    ? 'https://i.imgur.com/cqAklTF_d.webp'
-                                    : 'https://i.imgur.com/rpQdRoY_d.webp?maxwidth=728&fidelity=grand';
+                    let index = 0;
+                    for(let char of inText) {
+                        if(char != / +/) {
+                            switch(index % 2 == 0) {
+                                case true: {resultText += char.toLowerCase(); break;}
+                                case false: {resultText += char.toUpperCase(); break;}
                             }
-                        }
-                    ])
-                );
+                        } else {resultText += char;}
+                        index += 1;
+                    }
+                    return {
+                        send: true,
+                        result: resultText
+                    }
+                }
             }
-
-            return res;
-        });
+        );
     }
 
     pluginWillUnload() {
-        // When the plugin is unloaded, we need to unregister/uninject anything we've registered/injected.
-        powercord.api.settings.unregisterSettings(this.entityID);
-        uninject('my-plugin-image-menu');
+        powercord.api.settings.unregisterCommand('woke');
     }
 };
